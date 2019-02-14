abf053319c654c40ad9ab8bebe70a7e1baca2178
MACKENZIE HUTCHISON <mack2@uw.edu>
2019-02-14 00:54:49 -0800

    build report

--- analysis.R
+++ analysis.R
@@ -11,29 +11,27 @@ evictions <- read.csv("data/Eviction_Notices.csv", stringsAsFactors = F)
 # Compute some values of interest and store them in variables for the report
 
 # How many evictions were there?
-num_evicitions <- nrow(evictions)
+num_evictions <- nrow(evictions)
 num_features <- ncol(evictions)
 
 # Create a table (data frame) of evictions by zip code (sort descending)
-by_zip <- evictions %>% 
+evictions_by_zip <- evictions %>% 
   group_by(Eviction.Notice.Source.Zipcode) %>% 
   count() %>% 
-  arrange(-n) %>% 
-  ungroup() %>% 
-  top_n(10, wt = n)
+  arrange(-n)
 
 # Create a plot of the number of evictions each month in the dataset
 by_month <- evictions %>% 
   mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>% 
-  mutate(month = floor_date(date, unit="month")) %>% 
+  mutate(month = floor_date(date, unit = "month")) %>% 
   group_by(month) %>% 
   count()
 
 
 # Store plot in a variable
-month_plot <- ggplot(data = by_month) + 
-  geom_line(mapping = aes(x = month, y = n), color = "blue", alpha = .5) +
-  labs(x = "Date", y = "Number of Evictions", title = "Evictions over time in SF")
+month_plot <- ggplot(data = by_month) +
+  geom_line(mapping = aes(x = month, y = n)) +
+  labs(x = "Month", y = "Number of Evictions", title = "Evictions over time")
 
 # Map evictions in 2017 
 evictions_2017 <- evictions %>% 
@@ -44,10 +42,10 @@ evictions_2017 <- evictions %>%
     lat = as.numeric(gsub("\\(", "", lat)), # remove starting parentheses
     long = as.numeric(gsub("\\)", "", long)) # remove closing parentheses
   ) 
-
 # Format the lat/long variables, filter to 2017
 
 # Create a maptile background
+
 base_plot <- qmplot(
   data = evictions_2017,        # name of the data frame
   x = long,                     # data feature for longitude
